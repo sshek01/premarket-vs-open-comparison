4670341962202449e39c5681ee909a555a4f8aad
sshek01 <msheka01@gmail.com>
2026-09-16 23:26:16 -0500

    Final commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import yfinance as yf
 from datetime import datetime, timezone
+import os
+import mysql.connector
+from dotenv import load_dotenv
 
 hype_url = "https://api.hyperliquid.xyz/info"
 
@@ -51,13 +54,14 @@ aaplMids= price_info("xyz:AAPL", exactStart, exactEnd)
 
 #START YFINANCE 
 priceList = []
-datesList = []
+
+ticker = "AAPL"
 
 aapl = yf.Ticker("AAPL")
 
 #Grab Dates and Open Prices
 data = aapl.history(start = "2026-8-10", end = "2026-9-12")
-dates = data.index.strftime('%m-%d').tolist()
+dates = data.index.strftime('%Y-%m-%d').tolist()
 openPrice = data['Open'].tolist()
 
 for i in openPrice:
@@ -70,9 +74,34 @@ for i in range(24):
     gap = ((priceList[i] - aaplMids[i]) / aaplMids[i]) * 100
     priceGap.append(f"{gap:.2f}")
 
+#SQL connection
 
+load_dotenv()
 
+connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT", 3306))
+)
 
+cursor = connection.cursor()
 
+data_to_insert = list(zip(dates, [ticker] * len(dates), priceList, aaplMids, priceGap))
 
+insert_query = """
+INSERT INTO self_project_data (dates, ticker, priceList, aaplMids, priceGap)
+VALUES (%s, %s, %s, %s, %s)
+"""
 
+try:
+    cursor.executemany(insert_query, data_to_insert)
+    connection.commit()
+    print(f"Successfully inserted {cursor.rowcount} rows into MySQL.")
+except mysql.connector.Error as err:
+    print(f"Database error: {err}")
+    connection.rollback()
+finally:
+    cursor.close()
+    connection.close()
